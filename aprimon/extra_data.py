dac3f29276106ece95c9341dc60a2b938769a571
Jonathan Yong <yongrenjie@gmail.com>
2022-05-08 00:35:31 +0100

    user

--- aprimon/extra_data.py
+++ aprimon/extra_data.py
@@ -47,4 +47,36 @@ MORE_SPREADSHEETS = {
             "verify_method": "checkbox",
         },
     },
+
+    "jennweiteh": {
+        "swsh": {
+            "type": "grid",
+            "key": "1WDm2b9xH71BsY_spEjJ7dQD4NsrZ5c_197xOIb778Gg",
+            "tab_name": "Aprimons",
+            "pokemon_column": "E",
+            "ball_columns": "NMFGHIJKLOP",
+            "verify_method": "checkbox",
+        },
+    },
+    "jennweiteh (on-hands)": {
+        "swsh": {
+            "type": "grid",
+            "key": "1WDm2b9xH71BsY_spEjJ7dQD4NsrZ5c_197xOIb778Gg",
+            "tab_name": "On-Hands",
+            "pokemon_column": "E",
+            "ball_column": "C",
+        },
+    },
+
+    "OnePointPi (master sheet)": {
+        "swsh": {
+            "type": "grid",
+            "key": "15g1ubG103W077b6RoeVYDLryJWklNJRJzRINDqvKyn8",
+            "tab_name": "Master",
+            "pokemon_column": "C",
+            "ball_columns": "MLEFGHIJKNO",
+            "verify_method": "checkbox",
+            "use_formula": False,
+        },
+    },
 }
